9e8666d57bd585b269daff0588849a9941421ad2
mimikonadeshiko <67324520+mimikonadeshiko@users.noreply.github.com>
2022-03-17 10:59:48 +0900

    Add files via upload

--- text.py
+++ text.py
@@ -1,5 +1,10 @@
+#V1.0
 import mimikodev
 path = "問題.txt"
 with open(path) as f:
     mondai = [s.strip() for s in f.readlines()]
-mimikodev.dedenn(mondai)
+print("何回遊ぶの?")
+kaisuu = input()
+print("へぇー" + kaisuu + "回遊ぶんだー")
+for i in range(int (kaisuu)):
+    mimikodev.dedenn(mondai)
